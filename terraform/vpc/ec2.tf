32a62e4e431c0691cc6d61ba57795c81708076ea
FranciscoSantos05 <franciscolopesantos@outlook.pt>
2026-08-25 15:03:45 +0100

    bump instance to t3.medium for k3s memory headroom

--- terraform/vpc/ec2.tf
+++ terraform/vpc/ec2.tf
@@ -20,7 +20,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "main" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = "t3.medium"
 
   subnet_id                  = aws_subnet.public.id
   vpc_security_group_ids     = [aws_security_group.web.id]
