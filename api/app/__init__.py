d3178a5a2e4642d331e354445daece1b2c4b498f
FranciscoSantos05 <franciscolopesantos@outlook.pt>
2026-08-29 21:42:08 +0100

    expose Prometheus metrics from the Flask app - fix 2

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -16,7 +16,7 @@ def create_app(config_name=None):
     app.config.from_object(config_by_name[config_name])
 
     db.init_app(app)
-    metrics = PrometheusMetrics(app)
+    PrometheusMetrics(app)
     migrate.init_app(app, db)
 
     with app.app_context():
